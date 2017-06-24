d87ff14771416ce6e93936934c1786b3a1d1eba2
Nik <nerdvestor@gmail.com>
2017-06-23 19:23:12 -0700

    add devise funcionality

--- app/controllers/stock_values_controller.rb
+++ app/controllers/stock_values_controller.rb
@@ -1,4 +1,5 @@
 class StockValuesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_stock_value, only: [:show, :edit, :update, :destroy]
 
   # GET /stock_values
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :stock_values
   root 'new_age#index'
   get 'static_pages/index'
